4a392de0ea65318c31cf576668e7d9dd752b0453
abhaysharma419 <abhaysharmagk@gmail.com>
2018-01-17 23:40:13 +0530

    Update twitter_bot.py

--- py_bot/twitter_bot.py
+++ py_bot/twitter_bot.py
@@ -2,11 +2,11 @@ import tweepy
 import os
 import time
 
-consumer_key='H5jys7BWwEHON4Ca4i5PnLGiq'
-consumer_secret='ZDof7N71abIZB91U42OVRSBjiJsP9m6vrvUkd5Pkw4naq0NPSg'
+consumer_key=''
+consumer_secret=''
 
-access_token='953634970827874304-x60xHcoMOvOrS2SBIlV8pVlhugnXuGL'
-access_token_secret='RmVKvFvz2aQ8P5cLUy18UacSbeg0KQTT7uc3kNx5m8lW0'
+access_token=''
+access_token_secret=''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
